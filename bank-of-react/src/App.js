88cd0c7776f4aa094859bbdca84661252dd058f3
ButterUdder <Geoffrey.Huang27@myhunter.cuny.edu>
2021-11-17 16:01:18 -0500

    linking to routes

--- bank-of-react/src/App.js
+++ bank-of-react/src/App.js
@@ -1,24 +1,33 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './components/Home';
+import UserProfile from './components/UserProfile';
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      accountBalance: 14568.27
+      accountBalance: 14568.27,
+      currentUser: {
+        userName: 'joe_shmo',
+        memberSince: '07/23/96',
+      }
     }
   }
 
   render () {
     const HomeComponent = () => (<Home accountBalance={this.state.accountBalance}/>);
-    
+    const UserProfileComponent = () => (
+      <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince} />
+    );
+
     return (
       <Router>
-        <Switch>
+        <div>
           <Route exact path="/" render={HomeComponent}/>
-        </Switch>
+          <Route exact path="/userProfile" render={UserProfileComponent}/>
+        </div>
       </Router>
     );
   }
