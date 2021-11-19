8bcd7a537ba9d5e7b1812c1530fd735df824ea66
ButterUdder <Geoffrey.Huang27@myhunter.cuny.edu>
2021-11-19 11:04:10 -0500

    added form

--- bank-of-react/src/components/Credits.js
+++ bank-of-react/src/components/Credits.js
@@ -3,6 +3,9 @@ import {Link} from 'react-router-dom';
 import AccountBalance from './AccountBalance';
 
 const Credits = (props) => {
+    const [item, updateName] = useState('');
+    const [cost, updateCost] = useState('');
+
     let creditsView = () => {
         const { credits } = props;
         return credits.map((credit) => {
@@ -10,17 +13,37 @@ const Credits = (props) => {
             return <li key={credit.id}>{credit.amount} {credit.description} {date}</li>
         })
     }
+
+    const handleSubmit = (e) =>  {
+        e.preventDefault();
+        const newDebit = {item, cost};
+        props.addDebit(newDebit)
+        e.target.reset();
+    }
+
     return (
         <div>
             <h1> Credits </h1>
             {creditsView()}
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Credit Name
+                </label>
+                <input type="text" name="item" onChange={(e) => updateName(e.target.value)}/>
+                <label>
+                    Amount
+                </label>
+                <input type="text" name="cost" onChange={(e) => updateCost(e.target.value)}/>
+                <button>Add</button>
+            </form>
             <Link to="/">Home</Link>
             <Link to="/userProfile">User Profile</Link>
             <Link to="/login">Login</Link>
             <Link to="/debits">Debits</Link>
             <AccountBalance accountBalance={props.accountBalance}/>
+            <p>{ item }</p>
+            <p>{ cost }</p>
         </div>
-
     )
 }
 export default Credits;
